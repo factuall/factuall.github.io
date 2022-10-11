109047ea07b14e807a91fdb33e0c5700843cdb80
Adrian Nieściur <63786641+factuall@users.noreply.github.com>
2022-10-11 06:29:30 +0200

    better full-name animation

--- react-app/src/js/App.js
+++ react-app/src/js/App.js
@@ -79,7 +79,23 @@ const WebContent = () => {
   return (
     <div>
       <div className="NameHolder">
-        <p className={state.page == 0 ?  "FullName Show" : "FullName Hide"}>Adrian Nieściur</p>
+        <p className={state.page == 0 ?  "FullName Show" : "FullName Hide"}>
+          <div className="name-letter">A</div>
+          <div className="name-letter">d</div>
+          <div className="name-letter">r</div>
+          <div className="name-letter">i</div>
+          <div className="name-letter">a</div>
+          <div className="name-letter">n</div>
+          <div className="name-letter">&nbsp;</div>
+          <div className="name-letter">N</div>
+          <div className="name-letter">i</div>
+          <div className="name-letter">e</div>
+          <div className="name-letter">ś</div>
+          <div className="name-letter">c</div>
+          <div className="name-letter">i</div>
+          <div className="name-letter">u</div>
+          <div className="name-letter">r</div>
+        </p>
       </div>
       <div className={state.page > 0 ? "TopObstruction" : "TopObstruction Down"}></div>
       <div className={state.page > 0 ? "NavBar" : "NavBar Down"}> 
